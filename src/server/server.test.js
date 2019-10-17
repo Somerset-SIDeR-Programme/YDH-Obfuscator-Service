c6d17e4b7052ddd97da13eb468c0480a49596469
Frazer Smith <frazer.smith@ydh.nhs.uk>
2019-10-17 12:13:52 +0100

    Add new collection of tests

--- src/server/server.test.js
+++ src/server/server.test.js
@@ -8,17 +8,66 @@ const params = {
 	patient: 'https://fhir.nhs.uk/Id/nhs-number|9467335646&birthdate=1932-04-15',
 	practitioner: 'https://sider.nhs.uk/auth|frazer.smith@ydh.nhs.uk'
 };
-const path = `http://127.0.0.1:${serverConfig.port}`;
+
+describe('Server deployment', () => {
+	const port = '8204';
+	const path = `http://127.0.0.1:${port}`;
+	beforeAll(async () => {
+		jest.setTimeout(300000);
+	});
+
+	test('Should assign default values if none provided', async () => {
+		const server = new Server()
+			.configureRoute(obfuscationConfig.obfuscation)
+			.listen(port);
+		expect(server.config.protocol).toBe('http');
+		await server.shutdown();
+	});
+
+	test('Should fail if obfuscation config missing', async () => {
+		const server = new Server(serverConfig)
+			.configureRoute()
+			.listen(port);
+
+		const response = await request(path)
+			.get('')
+			.set('Content-Type', 'application/json')
+			.set('cache-control', 'no-cache')
+			.query(params);
+
+		expect(response.statusCode).toBe(500);
+
+		await server.shutdown();
+	});
+
+	test('Should set protocol to https', async () => {
+		const httpsServerConfig = {
+			https: true
+		};
+
+		try {
+			const server = new Server(httpsServerConfig)
+				.configureRoute(obfuscationConfig.obfuscation)
+				.listen(port);
+
+			expect(server.config.protocol).toBe('https');
+		} catch (e) {
+			// Do nothing
+		}
+	});
+});
 
 describe('Redirects', () => {
 	let server;
+	const port = '8205';
+	const path = `http://127.0.0.1:${port}`;
 
 	beforeAll(async () => {
 		jest.setTimeout(300000);
 		// Stand up server
 		server = new Server(serverConfig)
 			.configureRoute(obfuscationConfig.obfuscation)
-			.listen(serverConfig.port);
+			.listen(port);
 	});
 
 	afterAll(async () => {
@@ -55,5 +104,4 @@ describe('Redirects', () => {
 			expect(response.text).toBe('An essential parameter is missing');
 		}));
 	});
-});
 });
