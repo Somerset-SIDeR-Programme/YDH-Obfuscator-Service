f406738b7c08886f05e7308dfc1f9eb2561f8571
Frazer Smith <frazer.smith@ydh.nhs.uk>
2019-11-21 15:20:13 +0000

    Chain new middleware function calls

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const { obfuscationConfig, serverConfig } = require('./config');
+const { keycloakRetrieveConfig } = require('./keycloak-retrieve.config');
 const Server = require('./server/server');
 
 new Server(serverConfig)
-	.configureRoute(obfuscationConfig.obfuscation)
+	.configureKeycloakRetrival(keycloakRetrieveConfig)
+	.configureObfuscation(obfuscationConfig.obfuscation)
+	.configureRoute()
 	.listen(serverConfig.port);
