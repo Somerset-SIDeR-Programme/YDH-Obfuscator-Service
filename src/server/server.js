e0e9d2e0697ee10c0c9530c644b9dca1751d7979
Frazer Smith <frazer.smith@ydh.nhs.uk>
2019-11-21 09:35:59 +0000

    Push obfuscation out to own middleware

--- src/server/server.js
+++ src/server/server.js
@@ -3,24 +3,10 @@ const fs = require('fs');
 const helmet = require('helmet');
 const https = require('https');
 const http = require('http');
-const { obfuscate } = require('obfuscated-querystring/lib');
 
 const keycloakRetrieve = require('./middleware/keycloak-retrieve.middleware');
+const obfuscate = require('./middleware/obfuscate.middleware');
 const keycloakConfig = require('../keycloak-retrieve.config');
-
-function serialise(obj, prefix) {
-	const str = [];
-	Object.keys(obj).forEach((key) => {
-		if (Object.prototype.hasOwnProperty.call(obj, key)) {
-			const k = prefix ? `${prefix}[${key}]` : key;
-			const value = obj[key];
-			str.push((value !== null && typeof value === 'object')
-				? serialise(value, k)
-				: `${encodeURIComponent(k)}=${encodeURIComponent(value)}`);
-		}
-	});
-	return str.join('&');
-}
 
 class Server {
 	/**
@@ -53,19 +39,12 @@ class Server {
 	 * @returns {this} self
 	 */
 	configureRoute(options) {
-		this.app.get('/', keycloakRetrieve(keycloakConfig.keycloakRetrieveConfig), (req, res) => {
-			// Retrieve all param keys from query and check all essential ones are present
-			const keys = Object.keys(req.query);
-
-			// eslint-disable-next-line max-len
-			if (options.requiredParams.every((element) => keys.map((x) => x.toLowerCase()).includes(element.toLowerCase()))) {
-				const obfuscatedParams = obfuscate(serialise(req.query), options);
-				const espUrl = `https://pyrusapps.blackpear.com/esp/#!/launch?${obfuscatedParams}`;
-				console.log(espUrl);
-				res.redirect(espUrl);
-			} else {
-				res.status(400).send('An essential parameter is missing');
-			}
+		this.app.get('/', keycloakRetrieve(keycloakConfig.keycloakRetrieveConfig), obfuscate(options), (req, res, next) => {
+			// eslint-disable-next-line no-underscore-dangle
+			const espUrl = `https://pyrusapps.blackpear.com/esp/#!/launch?${req._parsedUrl.query}`;
+			console.log(espUrl);
+			res.redirect(espUrl);
+			next();
 		});
 
 		// return self for chaining
