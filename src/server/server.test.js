fff7bfa8a1cabc66194ca8002ed0aa0e6d6a4237
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-01-09 10:33:55 +0000

    Format with Prettier

--- src/server/server.test.js
+++ src/server/server.test.js
@@ -3,146 +3,149 @@ const { obfuscationConfig, serverConfig } = require('../config');
 const Server = require('./server');
 
 const params = {
-  birthdate: '1932-04-15',
-  location: 'https://fhir.nhs.uk/Id/ods-organization-code|RA4',
-  patient: 'https://fhir.nhs.uk/Id/nhs-number|9467335646&birthdate=1932-04-15',
-  practitioner: 'https://sider.nhs.uk/auth|frazer.smith@ydh.nhs.uk'
+	birthdate: '1932-04-15',
+	location: 'https://fhir.nhs.uk/Id/ods-organization-code|RA4',
+	patient:
+		'https://fhir.nhs.uk/Id/nhs-number|9467335646&birthdate=1932-04-15',
+	practitioner: 'https://sider.nhs.uk/auth|frazer.smith@ydh.nhs.uk'
 };
 
 describe('Server deployment', () => {
-  const port = '8204';
-  const path = `http://127.0.0.1:${port}`;
-  beforeAll(async () => {
-    jest.setTimeout(30000);
-  });
+	const port = '8204';
+	const path = `http://127.0.0.1:${port}`;
+	beforeAll(async () => {
+		jest.setTimeout(30000);
+	});
 
-  test('Should assign default values if none provided', async () => {
-    const server = new Server()
-      .configureObfuscation(obfuscationConfig.obfuscation)
-      .configureRoute()
-      .listen(port);
-    expect(server.config.protocol).toBe('http');
-    await server.shutdown();
-  });
+	test('Should assign default values if none provided', async () => {
+		const server = new Server()
+			.configureObfuscation(obfuscationConfig.obfuscation)
+			.configureRoute()
+			.listen(port);
+		expect(server.config.protocol).toBe('http');
+		await server.shutdown();
+	});
 
-  test('Should fail if obfuscation config missing', async () => {
-    const server = new Server(serverConfig)
-      .configureObfuscation()
-      .configureRoute()
-      .listen(port);
+	test('Should fail if obfuscation config missing', async () => {
+		const server = new Server(serverConfig)
+			.configureObfuscation()
+			.configureRoute()
+			.listen(port);
 
-    const response = await request(path)
-      .get('')
-      .set('Content-Type', 'application/json')
-      .set('cache-control', 'no-cache')
-      .query(params);
+		const response = await request(path)
+			.get('')
+			.set('Content-Type', 'application/json')
+			.set('cache-control', 'no-cache')
+			.query(params);
 
-    expect(response.statusCode).toBe(500);
+		expect(response.statusCode).toBe(500);
 
-    await server.shutdown();
-  });
+		await server.shutdown();
+	});
 
-  test('Should set protocol to https', async () => {
-    const httpsServerConfig = {
-      https: true
-    };
+	test('Should set protocol to https', async () => {
+		const httpsServerConfig = {
+			https: true
+		};
 
-    try {
-      const server = new Server(httpsServerConfig)
-        .configureObfuscation(obfuscationConfig.obfuscation)
-        .configureRoute()
-        .listen(port);
+		try {
+			const server = new Server(httpsServerConfig)
+				.configureObfuscation(obfuscationConfig.obfuscation)
+				.configureRoute()
+				.listen(port);
 
-      expect(server.config.protocol).toBe('https');
-    } catch (error) {
-      // Do nothing
-    }
-  });
+			expect(server.config.protocol).toBe('https');
+		} catch (error) {
+			// Do nothing
+		}
+	});
 });
 
 describe('Redirects', () => {
-  let server;
-  const port = '8205';
-  const path = `http://127.0.0.1:${port}`;
+	let server;
+	const port = '8205';
+	const path = `http://127.0.0.1:${port}`;
 
-  beforeAll(async () => {
-    jest.setTimeout(30000);
-    // Stand up server
-    server = new Server(serverConfig)
-      .configureObfuscation(obfuscationConfig.obfuscation)
-      .configureRoute()
-      .listen(port);
-  });
+	beforeAll(async () => {
+		jest.setTimeout(30000);
+		// Stand up server
+		server = new Server(serverConfig)
+			.configureObfuscation(obfuscationConfig.obfuscation)
+			.configureRoute()
+			.listen(port);
+	});
 
-  afterAll(async () => {
-    try {
-      await server.shutdown();
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  });
+	afterAll(async () => {
+		try {
+			await server.shutdown();
+		} catch (error) {
+			console.log(error);
+			throw error;
+		}
+	});
 
-  test('Should redirect to Black Pear\'s ESP with all params present', async () => {
-    const response = await request(path)
-      .get('')
-      .set('Content-Type', 'application/json')
-      .set('cache-control', 'no-cache')
-      .query(params);
+	test("Should redirect to Black Pear's ESP with all params present", async () => {
+		const response = await request(path)
+			.get('')
+			.set('Content-Type', 'application/json')
+			.set('cache-control', 'no-cache')
+			.query(params);
 
-    expect(response.statusCode).toBe(302);
-    expect(response.headers.location.substring(0, 46)).toBe('https://pyrusapps.blackpear.com/esp/#!/launch?');
-  });
+		expect(response.statusCode).toBe(302);
+		expect(response.headers.location.substring(0, 46)).toBe(
+			'https://pyrusapps.blackpear.com/esp/#!/launch?'
+		);
+	});
 
-  test('Should fail to redirect when any required param is missing', async () => {
-    await Promise.all(
-      Object.keys(params).map(async (key) => {
-        const alteredParams = { ...params };
-        delete alteredParams[key];
-        const response = await request(path)
-          .get('')
-          .set('Content-Type', 'application/json')
-          .set('cache-control', 'no-cache')
-          .query(alteredParams);
+	test('Should fail to redirect when any required param is missing', async () => {
+		await Promise.all(
+			Object.keys(params).map(async (key) => {
+				const alteredParams = { ...params };
+				delete alteredParams[key];
+				const response = await request(path)
+					.get('')
+					.set('Content-Type', 'application/json')
+					.set('cache-control', 'no-cache')
+					.query(alteredParams);
 
-        expect(response.statusCode).toBe(400);
-        expect(response.text).toBe('An essential parameter is missing');
-      })
-    );
-  });
+				expect(response.statusCode).toBe(400);
+				expect(response.text).toBe('An essential parameter is missing');
+			})
+		);
+	});
 });
 
 describe('Keycloak token retrival', () => {
-  let server;
-  const port = '8206';
-  const path = `http://127.0.0.1:${port}`;
+	let server;
+	const port = '8206';
+	const path = `http://127.0.0.1:${port}`;
 
-  beforeAll(async () => {
-    jest.setTimeout(30000);
-    // Stand up server
-    server = new Server(serverConfig)
-      .configureKeycloakRetrival()
-      .configureObfuscation(obfuscationConfig.obfuscation)
-      .configureRoute()
-      .listen(port);
-  });
+	beforeAll(async () => {
+		jest.setTimeout(30000);
+		// Stand up server
+		server = new Server(serverConfig)
+			.configureKeycloakRetrival()
+			.configureObfuscation(obfuscationConfig.obfuscation)
+			.configureRoute()
+			.listen(port);
+	});
 
-  afterAll(async () => {
-    try {
-      await server.shutdown();
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  });
+	afterAll(async () => {
+		try {
+			await server.shutdown();
+		} catch (error) {
+			console.log(error);
+			throw error;
+		}
+	});
 
-  test('Should fail if Keycloak endpoint config missing', async () => {
-    const response = await request(path)
-      .get('')
-      .set('Content-Type', 'application/json')
-      .set('cache-control', 'no-cache')
-      .query(params);
+	test('Should fail if Keycloak endpoint config missing', async () => {
+		const response = await request(path)
+			.get('')
+			.set('Content-Type', 'application/json')
+			.set('cache-control', 'no-cache')
+			.query(params);
 
-    expect(response.statusCode).toBe(500);
-  });
+		expect(response.statusCode).toBe(500);
+	});
 });
