9a8abfdee5403ca022cc6a5c9b93f3729fbf374d
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-04-17 13:00:29 +0100

    refactor(routes): replace deprecated request module with axios

--- src/server/middleware/keycloak.middleware.js
+++ src/server/middleware/keycloak.middleware.js
@@ -1,4 +1,5 @@
-const request = require('request-promise');
+const request = require('axios');
+const queryString = require('query-string');
 
 /**
  * @author Frazer Smith
@@ -12,8 +13,6 @@ const request = require('request-promise');
  * @param {String} config.serviceAuthorisation.form.grant_type
  * @param {String} config.serviceAuthorisation.form.password
  * @param {String} config.serviceAuthorisation.form.username
- * @param {Object} config.serviceAuthorisation.options
- * @param {Object} config.serviceAuthorisation.options.headers
  * @param {String} config.serviceAuthorisation.url
  *
  * @param {Object} config.requestToken
@@ -24,8 +23,6 @@ const request = require('request-promise');
  * @param {String} config.requestToken.form.grant_type
  * @param {String} config.requestToken.form.request_subject
  * @param {String} config.requestToken.form.request_token_type
- * @param {Object} config.requestToken.options
- * @param {Object} config.requestToken.options.headers
  * @param {String} config.requestToken.url
  *
  * @return {Function} Express middleware.
@@ -38,29 +35,37 @@ module.exports = function keycloakMiddleware(config = {}) {
 			Object.keys(config).length === 0
 		) {
 			next();
+		} else {
+			try {
+				const { requestToken, serviceAuthorisation } = config;
+	
+				// Service authorisation to retrieve subject access token
+				const serviceAuthResponse = await request.post(
+					serviceAuthorisation.url,
+					queryString.stringify(serviceAuthorisation.form)
+				);
+	
+				requestToken.form.subject_token =
+					serviceAuthResponse.data.access_token;
+	
+				// Expects the practitioner query to be in [system]|[code] format
+				requestToken.form.requested_subject = req.query.practitioner.split(
+					'|'
+				)[1];
+	
+				// Request access token for user
+				const userAccessResponse = await request.post(
+					requestToken.url,
+					queryString.stringify(requestToken.form)
+				);
+				req.query.access_token = userAccessResponse.data.access_token;
+				next();
+			} catch (error) {
+				res.status(500);
+				next(new Error(error));
+			}
 		}
 
-		try {
-			const { requestToken, serviceAuthorisation } = config;
 
-			// Service authorisation to retrieve subject access token
-			const serviceAuthResponse = await request.post(serviceAuthorisation);
-			requestToken.form.subject_token = JSON.parse(
-				serviceAuthResponse
-			).access_token;
-
-			// Expects the practitioner query to be in [system]|[code] format
-			requestToken.form.requested_subject = req.query.practitioner.split(
-				'|'
-			)[1];
-
-			// Request access token for user
-			const userAccessResponse = await request.post(requestToken);
-			req.query.access_token = JSON.parse(userAccessResponse).access_token;
-			next();
-		} catch (error) {
-			res.status(500);
-			next(new Error(error));
-		}
 	};
 };
