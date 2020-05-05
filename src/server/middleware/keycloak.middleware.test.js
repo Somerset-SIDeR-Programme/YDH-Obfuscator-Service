b182efd25c52c28c3025fe422aac227e5f55c0dd
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-05-05 19:55:10 +0100

    tests(middleware): extend test pass conditions

--- src/server/middleware/keycloak.middleware.test.js
+++ src/server/middleware/keycloak.middleware.test.js
@@ -49,12 +49,13 @@ describe('Keycloak middleware', () => {
 
 		await middleware(req, res, next);
 		expect(next).toHaveBeenCalledTimes(1);
+		expect(next.mock.calls[0][0]).toBeUndefined();
 		expect(req.query.access_token).toBe('mock-access-token-authorised');
-		expect(res.statusCode).not.toBe(500);
+		expect(res.statusCode).toBe(200);
 	});
 
-	test('Should throw error if connection issue encountered', async () => {
-		const altKeycloakConfig = { ...keycloakRetrieveConfig };
+	test('Should throw type error if url value missing when retrieving request token', async () => {
+		const altKeycloakConfig = JSON.parse(JSON.stringify(keycloakRetrieveConfig));
 		delete altKeycloakConfig.requestToken.url;
 
 		const middleware = keycloakMiddleware(altKeycloakConfig);
@@ -70,6 +71,78 @@ describe('Keycloak middleware', () => {
 
 		await middleware(req, res, next);
 		expect(next).toHaveBeenCalledTimes(1);
+		expect(next.mock.calls[0][0].message).toBe(
+			'TypeError [ERR_INVALID_ARG_TYPE]: The "url" argument must be of type string. Received undefined'
+		);
+		expect(res.statusCode).toBe(500);
+	});
+
+	test('Should throw type error if url value missing when making service authorisation', async () => {
+		const altKeycloakConfig = JSON.parse(JSON.stringify(keycloakRetrieveConfig));
+		delete altKeycloakConfig.serviceAuthorisation.url;
+
+		const middleware = keycloakMiddleware(altKeycloakConfig);
+
+		const req = httpMocks.createRequest({
+			query: {
+				practitioner:
+					'https://sider.ydh.nhs.uk/auth|frazer.smith@ydh.nhs.uk'
+			}
+		});
+		const res = httpMocks.createResponse();
+		const next = jest.fn();
+
+		await middleware(req, res, next);
+		expect(next).toHaveBeenCalledTimes(1);
+		expect(next.mock.calls[0][0].message).toBe(
+			'TypeError [ERR_INVALID_ARG_TYPE]: The "url" argument must be of type string. Received undefined'
+		);
+		expect(res.statusCode).toBe(500);
+	});
+
+	test('Should throw error if connection issue encountered when retrieving request token', async () => {
+		const altKeycloakConfig = JSON.parse(JSON.stringify(keycloakRetrieveConfig));
+		altKeycloakConfig.requestToken.url = 'test';
+
+		const middleware = keycloakMiddleware(altKeycloakConfig);
+
+		const req = httpMocks.createRequest({
+			query: {
+				practitioner:
+					'https://sider.ydh.nhs.uk/auth|frazer.smith@ydh.nhs.uk'
+			}
+		});
+		const res = httpMocks.createResponse();
+		const next = jest.fn();
+
+		await middleware(req, res, next);
+		expect(next).toHaveBeenCalledTimes(1);
+		expect(next.mock.calls[0][0].message).toBe(
+			'Error: connect ECONNREFUSED 127.0.0.1:80'
+		);
+		expect(res.statusCode).toBe(500);
+	});
+
+	test('Should throw error if connection issue encountered when making service authorisation', async () => {
+		const altKeycloakConfig = JSON.parse(JSON.stringify(keycloakRetrieveConfig));
+		altKeycloakConfig.serviceAuthorisation.url = 'test';
+
+		const middleware = keycloakMiddleware(altKeycloakConfig);
+
+		const req = httpMocks.createRequest({
+			query: {
+				practitioner:
+					'https://sider.ydh.nhs.uk/auth|frazer.smith@ydh.nhs.uk'
+			}
+		});
+		const res = httpMocks.createResponse();
+		const next = jest.fn();
+
+		await middleware(req, res, next);
+		expect(next).toHaveBeenCalledTimes(1);
+		expect(next.mock.calls[0][0].message).toBe(
+			'Error: connect ECONNREFUSED 127.0.0.1:80'
+		);
 		expect(res.statusCode).toBe(500);
 	});
 });
