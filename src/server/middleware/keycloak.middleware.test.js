1e0855959045809fa83325af75d21a30f2d346eb
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-05-05 15:30:03 +0100

    tests(middleware): add tests using mock keycloak server and config

--- src/server/middleware/keycloak.middleware.test.js
+++ src/server/middleware/keycloak.middleware.test.js
@@ -1,7 +1,24 @@
 const httpMocks = require('node-mocks-http');
 const keycloakMiddleware = require('./keycloak.middleware');
+const {
+	keycloakRetrieveConfig
+} = require('../../../mocks/keycloak-config.mock');
+const mockKeycloakServer = require('../../../mocks/keycloak-server.mock');
 
 describe('Keycloak middleware', () => {
+	afterAll(async () => {
+		// Shutdown mock Keycloak server
+		try {
+			await mockKeycloakServer.close();
+			setImmediate(() => {
+				mockKeycloakServer.emit('close');
+			});
+		} catch (error) {
+			console.log(error);
+			throw error;
+		}
+	});
+
 	test('Should return a middleware function', () => {
 		const middleware = keycloakMiddleware();
 		expect(typeof middleware).toBe('function');
@@ -16,5 +33,43 @@ describe('Keycloak middleware', () => {
 
 		middleware(req, res, next);
 		expect(next).toHaveBeenCalledTimes(1);
+	});
+
+	test('Should return access_token', async () => {
+		const middleware = keycloakMiddleware(keycloakRetrieveConfig);
+
+		const req = httpMocks.createRequest({
+			query: {
+				practitioner:
+					'https://sider.ydh.nhs.uk/auth|frazer.smith@ydh.nhs.uk'
+			}
+		});
+		const res = httpMocks.createResponse();
+		const next = jest.fn();
+
+		await middleware(req, res, next);
+		expect(next).toHaveBeenCalledTimes(1);
+		expect(req.query.access_token).toBe('mock-access-token-authorised');
+		expect(res.statusCode).not.toBe(500);
+	});
+
+	test('Should throw error if connection issue encountered', async () => {
+		const altKeycloakConfig = { ...keycloakRetrieveConfig};
+		delete altKeycloakConfig.requestToken.url;
+
+		const middleware = keycloakMiddleware(altKeycloakConfig);
+
+		const req = httpMocks.createRequest({
+			query: {
+				practitioner:
+					'https://sider.ydh.nhs.uk/auth|frazer.smith@ydh.nhs.uk'
+			}
+		});
+		const res = httpMocks.createResponse();
+		const next = jest.fn();
+
+		await middleware(req, res, next);
+		expect(next).toHaveBeenCalledTimes(1);
+		expect(res.statusCode).toBe(500);
 	});
 });
