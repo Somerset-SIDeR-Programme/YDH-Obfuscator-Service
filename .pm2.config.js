90e8db7fff56435201bb9de456f99b24b0ef939d
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-06-04 16:50:57 +0100

    chore: watch .env.production file for pm2 restarts

--- .pm2.config.js
+++ .pm2.config.js
@@ -1,14 +1,14 @@
 // Used by PM2 for deployment
 module.exports = {
-    apps : [{
+    apps: [{
         cwd: __dirname,
         env: {
             NODE_ENV: "production"
-          },
+        },
         exec_mode: 'cluster',
         instances: 4,
         name: "sider-obfu",
         script: './src/index.js',
-        watch: './src/config.js'
+        watch: ['./src/config.js', '.env.production']
     }]
 }
